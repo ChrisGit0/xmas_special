60e4e019663661fd8cee36341ef5c5f845082e76
ChrisGit0 <hristiqnshterev@gmail.com>
2023-11-27 21:12:13 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,262 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(@" 
+
+               #### ###
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               $*## $8#
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               F*$# Y##
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FU$* Y$*
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUC$ YO$**
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU*
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU//
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU?/
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU?
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU*
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU%
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU.
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU._ 
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU.|
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU!***
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU!**
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU!*
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU!
+
+");
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            Console.WriteLine(@" 
+
+               FUCK YOU!
+
+");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(@" 
+
+               FUCK YOU!
+
+");
+            System.Threading.Thread.Sleep(500);
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(@" 
+
+               FUCK YOU!
+
+");
+            System.Threading.Thread.Sleep(500);
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(@" 
+
+               FUCK YOU!
+
+");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(@" 
+
+               FUCK YOU!
+
+");
+            Console.Clear();
+            System.Threading.Thread.Sleep(1100);
+            Console.WriteLine( @"
+
+
+-" );
+            Console.Clear();
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine(@"
+
+
+
+-c");
+            Console.Clear();
+            System.Threading.Thread.Sleep(100);
+            Console.Clear();
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine(@"
+
+
+
+-ch");
+            Console.Clear();
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine(@"
+
+
+
+-chr");
+            Console.Clear();
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine(@"
+
+
+
+-chri");
+            Console.Clear();
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine(@"
+
+
+
+-chris");
+            Console.Clear();
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine(@"
+
+
+
+-chris");
+            System.Threading.Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("///////////MERRY+_+XMASS/////////////");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
